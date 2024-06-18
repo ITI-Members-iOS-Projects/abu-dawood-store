360888ec81cc41728cd4f96b733175b6fcb983fd
abdallah ismail <abdallahismailbmx6@gmail.com>
2024-06-18 18:59:17 +0300

    Login&chooseType Screen UI Added
    - Login&chooseType Screen UI Added
    - created custom yellow button will be used among all the App
    - Added core folder (entities,UseCases,GateWays)

---
+++ Project/Source/Utilities/Extensions/UI/UIColor+Extentions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extentions.swift
+//  Abu Dawood
+//
+//  Created by Abdallah ismail on 12/06/2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customGold = UIColor(red: 218/255, green: 158/255, blue: 25/255, alpha: 1.0)
+    static let customYellow = UIColor(red: 255/255, green: 221/255, blue: 0/255, alpha: 0.75)
+    static let shadowBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+    
+}
---
+++ Project/Source/ViewRelated/Modules/LoginModule/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Abu Dawood
+//
+//  Created by Abdallah ismail on 11/06/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var buttonView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonView.dropShadow()
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
