b1b9e6969aedf9e9a02012095cd8e086b7d47147
abdallah ismail <abdallahismailbmx6@gmail.com>
2024-06-19 21:16:28 +0300

    Login&chooseType Screen UI Added
    - Login&chooseType Screen UI Added
    - created custom yellow button will be used among all the App
    - Added core folder (entities,UseCases,GateWays)

---
+++ Project/Source/Utilities/Extensions/UI/UIColor+Extentions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extentions.swift
+//  Abu Dawood
+//
+//  Created by Abdallah ismail on 19/06/2024.
+//
+
+import Foundation
---
+++ Project/Source/ViewRelated/ReusableViews/GrediantYellowCustomButton.swift
@@ -0,0 +1,8 @@
+//
+//  GrediantYellowCustomButton.swift
+//  Abu Dawood
+//
+//  Created by Abdallah ismail on 19/06/2024.
+//
+
+import Foundation
